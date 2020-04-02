8e109638e21e65abfe61a34272621a3090315110
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-02 15:48:09 +1300

    Add @Component Annotation onto RestRepositoryConfigurator.java class   If no Annotation exist, Spring boot will ignore this as a component   and will NOT apply the configuration.

--- server/spring-boot-vue/src/main/java/com/example/springbootvue/RestRepositoryConfigurator.java
+++ server/spring-boot-vue/src/main/java/com/example/springbootvue/RestRepositoryConfigurator.java
@@ -2,8 +2,10 @@ package com.example.springbootvue;
 
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.stereotype.Component;
 
 
+@Component
 public class RestRepositoryConfigurator implements RepositoryRestConfigurer {
    @Override
    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
